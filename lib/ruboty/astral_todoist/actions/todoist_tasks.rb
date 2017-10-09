dac5ee7a6012290028caa09469dba8d61db4ed33
sanyaitiya <fragment.vertex@gmail.com>
2017-10-09 13:16:36 +0900

    [add] 日付を指定してタスクを取得できる

--- lib/ruboty/astral_todoist/actions/todoist_tasks.rb
+++ lib/ruboty/astral_todoist/actions/todoist_tasks.rb
@@ -5,7 +5,10 @@ module Ruboty
   module AstralTodoist
     module Actions
       class TodoistTasks < Ruboty::Actions::Base
-        @@lines = {:project_not_found => "Project not found"}
+        @@lines = {
+            :project_not_found => "Project not found",
+            :invalid_date => "Invalid date"
+        }
         @@client = Todoist::Client.create_client_by_token("#{ENV['TODOIST_TOKEN']}")
 
         def call
@@ -17,15 +20,34 @@ module Ruboty
         private
         def todoist_tasks(message)
           project_name = message.match_data.named_captures["project_name"]
+          specified_date = message.match_data.named_captures["date"]
 
           project = @@client.sync_projects.collection.select {|_, v| v.name == project_name}.values.first
           if project == nil
             return @@lines[:project_not_found]
           end
 
+
+          date_range =
+              case message.match_data.named_captures["date"]
+                when "all" then
+                  nil
+                when "today" then
+                  now = Time.now
+                  Time.local(now.year, now.month, now.day)..Time.local(now.year, now.month, now.day+1)
+                when "7days" then
+                  now = Time.now
+                  Time.local(now.year, now.month, now.day)..Time.local(now.year, now.month, now.day+8)
+                else
+                  return @@lines[:invalid_date]
+          end
+
           @@client.sync_items.collection.values.select do |v|
             # noinspection RubyResolve
             v.project_id == project.id
+          end.select do |v|
+            # noinspection RubyResolve
+            date_range == nil ? true : date_range.cover?(todoist_date_to_datetime(v.due_date_utc))
           end.sort do |a, b|
             # noinspection RubyResolve
             todoist_date_to_datetime(a.due_date_utc) <=> todoist_date_to_datetime(b.due_date_utc)
--- lib/ruboty/handlers/astral_todoist.rb
+++ lib/ruboty/handlers/astral_todoist.rb
@@ -4,7 +4,7 @@ module Ruboty
   module Handlers
     # Todoist plugin
     class AstralTodoist < Base
-      on /todoist tasks\s+(?<project_name>.*)/, name: 'todoist_tasks', description: 'list tasks'
+      on /todoist tasks\s+(?<date>.*)\s+(?<project_name>.*)/, name: 'todoist_tasks', description: 'list tasks'
       env :TODOIST_TOKEN, "Todoist account API token"
 
       def todoist_tasks(message)
