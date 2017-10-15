7c8e49f1774d60f604f2613122ea971525967919
sanyaitiya <fragment.vertex@gmail.com>
2017-10-15 20:56:14 +0900

    [add] 先頭のタスクを完了する機能を追加

---
+++ lib/ruboty/astral_todoist/actions/todoist_tasks_complete_first.rb
@@ -0,0 +1,46 @@
+require 'todoist'
+require 'time'
+require "ruboty/todoist_resource/items"
+
+module Ruboty
+  module AstralTodoist
+    module Actions
+      class TodoistTasksCompleteFirst < Ruboty::Actions::Base
+        @@lines = {
+            :project_not_found => "Project not found",
+            :invalid_date => "Invalid date"
+        }
+        @@client = Todoist::Client.create_client_by_token("#{ENV['TODOIST_TOKEN']}")
+
+        def call
+          message.reply(todoist_tasks_complete_first(message))
+        rescue => e
+          message.reply(e.message)
+        end
+
+        private
+        def todoist_tasks_complete_first(message)
+          project_name = message.match_data.named_captures["project_name"]
+          specified_date = message.match_data.named_captures["date"]
+
+          items = Ruboty::TodoistResource::Items.new(nil)
+          begin
+            first_content = items.filter_by_project(project_name).filter_by_due_date(specified_date).sort_by_due_date.items.first
+            if first_content == nil
+              "All task completed"
+            else
+              @@client.sync_items.complete([first_content])
+              @@client.sync
+              "complete #{first_content.content}"
+            end
+          rescue Ruboty::TodoistResource::ProjectNotFoundError
+            @@lines[:project_not_found]
+          rescue Ruboty::TodoistResource::InvalidDateError
+            @@lines[:invalid_date]
+          end
+        end
+
+      end
+    end
+  end
+end
--- lib/ruboty/handlers/astral_todoist.rb
+++ lib/ruboty/handlers/astral_todoist.rb
@@ -1,5 +1,6 @@
 require "ruboty/astral_todoist/actions/todoist_tasks"
 require "ruboty/astral_todoist/actions/todoist_tasks_first"
+require "ruboty/astral_todoist/actions/todoist_tasks_complete_first"
 
 module Ruboty
   module Handlers
@@ -7,6 +8,7 @@ module Ruboty
     class AstralTodoist < Base
       on /todoist tasks list\s+(?<date>.*)\s+(?<project_name>.*)/, name: 'todoist_tasks', description: 'list tasks'
       on /todoist tasks first\s+(?<date>.*)\s+(?<project_name>.*)/, name: 'todoist_tasks_first', description: 'take first task'
+      on /todoist tasks complete_first\s+(?<date>.*)\s+(?<project_name>.*)/, name: 'todoist_tasks_complete_first', description: 'complete first task'
       env :TODOIST_TOKEN, "Todoist account API token"
 
       def todoist_tasks(message)
@@ -15,6 +17,10 @@ module Ruboty
 
       def todoist_tasks_first(message)
         Ruboty::AstralTodoist::Actions::TodoistTasksFirst.new(message).call
+      end
+
+      def todoist_tasks_complete_first(message)
+        Ruboty::AstralTodoist::Actions::TodoistTasksCompleteFirst.new(message).call
       end
 
     end
